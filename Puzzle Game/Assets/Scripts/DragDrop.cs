edaecdca5c46c8d17fab15d403c77d18183b0cbb
bellapalumbi <bpalumbi@youth.dhftc.org>
2018-04-11 09:24:21 -0400

    Dragging and Dropping for Fire

---
+++ Puzzle Game/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour {
+
+	private Vector3 offset;
+	void OnMouseDown () {
+		//moves object in relation to the mouse drag by finding the 'offset' using camera.main.screentoworld point. The 10.0f is the distance from the cam to object
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+	}
+	
+	void OnMouseDrag() {
+		Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+		transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+	}
+}
